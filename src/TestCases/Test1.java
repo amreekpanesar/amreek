2cdc8a8c458347a67dea86422c5a40d738e95565
Amreek Panesar <Amreek Panesar>
2016-01-20 16:08:25 +0530

    First Commit

---
+++ src/TestCases/Test1.java
@@ -0,0 +1,20 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class Test1 {
+  
+	@Test
+	public void logintoyahoo() {
+		
+		Assert.assertEquals(1, 2);
+		Assert.assertTrue(message, condition);
+	}
+  
+  
+  
+  
+  
+}
